66c39808dfe674e505ff8c8e77688f776a7f426d
wryser <104174626+wryser@users.noreply.github.com>
2022-06-28 16:05:27 +1000

    Add files via upload

---
+++ Assets/Scripts/HouseDescriptor.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class HouseDescriptor : MonoBehaviour
+{
+    public string HouseName;
+    public string Author;
+
+    static private string prefabPath;
+
+    static public string filePath;
+}
